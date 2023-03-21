a4736f53f8cbd8da4fee238b0cc3c65f8af2b36c
Harshmars <105042579+Harshmars@users.noreply.github.com>
2023-03-21 22:16:43 +0530

    Create 1. program to find an element with it's position.c

---
+++ 1. program to find an element with it's position.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int size;
+    printf("enter size of the array :  ");
+    scanf("%d",&size);
+    int arr[size];
+    printf("enter elements :  ");
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    int key,flag=0;
+    printf("enter which element is to be removed from the array :  ");
+    scanf("%d",&key);
+    for(int i=0;i<size;i++){
+        if(arr[i]==key){
+           printf("%d exists at position %d",arr[i],i+1);
+           flag=1;
+           break;
+        }
+    }
+    if(flag ==0){
+        printf("element does not exists in the array");
+    }
+    return 0;
+}
