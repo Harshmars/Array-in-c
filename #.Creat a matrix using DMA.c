5cdd9c864cf4f4f217666b4a724e754d144a1cff
Harshmars <105042579+Harshmars@users.noreply.github.com>
2023-04-28 02:13:45 +0530

    Update #.Creat a matrix using DMA.c

--- #.Creat a matrix using DMA.c
+++ #.Creat a matrix using DMA.c
@@ -1,33 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
-    int **p,row,column;
-  
-    //--inputing row column from user--//
-    printf("Enter row of the matix :  ");
-    scanf("%d",&row);
-    printf("Enter column of the matix :  ");
-    scanf("%d",&column);
-  
-  
-    p=(int**)calloc(row,row);//creating p[0]..and..p[1]
-    p[0]=(int *)calloc(column,row);//creating p[0] indx1 indx2
-    p[1]=(int *)calloc(column,row);//creting p[1] indx1 indx2 indx3
-    
-    //--taking input from user--//
-    printf("Enter array Elements"):
+void input_arr(int** arr,int row,int column){
     for(int i=0;i<row;i++){
         for(int j=0;j<column;j++){
-            scanf("%d",&p[i][j]);
+            scanf("%d",&arr[i][j]);
         }
     }
-    
-    //--printing Elements--//
+}
+
+void print_arr(int **arr,int row,int column){
+    printf("The arrayu is :\n");
     for(int i=0;i<row;i++){
         for(int j=0;j<column;j++){
-            printf("%d\t",p[i][j]);
+            printf("%d ",arr[i][j]);
         }
         printf("\n");
     }
-  return 0;
+}
+
+int main(){
+    int row,column;
+    int **arr;
+    printf("Enter Matrix row :  ");
+    scanf("%d",&row);
+    printf("Enter matrix column :  ");
+    scanf("%d",&column);
+
+    arr=(int*)malloc(row*sizeof(int));
+    for(int i=0;i<row;i++){
+        arr[i]=(int*)malloc(column*sizeof(int));
+    }
+
+    input_arr(arr,row,column);
+    print_arr(arr,row,column);
 }
